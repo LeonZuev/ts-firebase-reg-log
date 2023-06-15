c2e17b7a06ce3bf26bf076cd69d9687542145974
leonzuev <loonatune@gmail.com>
2023-06-15 16:04:20 +0300

    Display usernames

--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import type User from '../../types/User';
 
 export default function UsersPage(): JSX.Element {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/users')
+      .then((res) => res.json())
+      .then((arr) => {
+        console.log(arr);
+        setUsers(arr);
+      });
+  }, []);
   return (
     <>
       <div>UsersPage</div>
+      {users.map((user) => <div>{user?.username}</div>)}
       <Outlet />
     </>
   );
