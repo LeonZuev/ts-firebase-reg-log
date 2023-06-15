d6153fbe08d6ec4b6b9e4d27af373d58740314b8
leonzuev <loonatune@gmail.com>
2023-06-15 15:04:00 +0300

    Add User.ts

---
+++ src/types/User.ts
@@ -0,0 +1,24 @@
+interface Name {
+  firstname: string,
+  lastname: string,
+}
+interface User {
+  id: number,
+  email: string,
+  username: string,
+  password: string,
+  name: Name,
+  // address: {
+  //   city: 'kilcoole',
+  //   street: '7835 new road',
+  //   number: 3,
+  //   zipcode: '12926-3874',
+  //   geolocation: {
+  //     lat: '-37.3159',
+  //     long: '81.1496',
+  //   }
+  // },
+  // phone: '1-570-236-7033'
+}
+
+export default User;
