ef3392bfd3f67453f5a9600f03c170afe1f24e7c
leonzuev <loonatune@gmail.com>
2023-06-17 20:21:12 +0300

    Add "delete todo"

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import {
   DocumentData, addDoc, collection,
+  deleteDoc,
+  doc,
   onSnapshot, orderBy, query, serverTimestamp,
 } from 'firebase/firestore';
 import { db } from '../../api/firebase';
@@ -12,12 +14,13 @@ export default function Todos(): JSX.Element {
   const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'));
   useEffect(() => {
     onSnapshot(q, (snapshot) => {
-      setTodos(snapshot.docs.map((doc: DocumentData) => ({
-        id: doc.id,
-        item: doc.data()
+      setTodos(snapshot.docs.map((document: DocumentData) => ({
+        id: document.id,
+        item: document.data()
       })));
     });
-  }, [input, q]);
+    console.log(todos);
+  }, [input]);
 
   const addTodo = (e: React.SyntheticEvent): void => {
     e.preventDefault();
@@ -41,10 +44,17 @@ export default function Todos(): JSX.Element {
         />
         <button type="submit">Add</button>
       </form>
-
       {todos.map((el) => (
         <div key={el.id}>
           {el.item.todo}
+          <button
+            type="button"
+            onClick={
+              () => { deleteDoc(doc(db, 'todos', el.id)); }
+            }
+          >
+            удалить
+          </button>
         </div>
       ))}
     </>
